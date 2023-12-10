5d5f658ae9a895ec383cba888a1fab57fcf36dec
Andreevich-Ivangit config --global user.name Andreevich-Ivangit config --global user.name Andreevich-Ivan <prostov2@gmail.com>
2023-12-10 23:35:56 +0300

    fix js

--- js/main.js
+++ js/main.js
@@ -3,47 +3,51 @@ const logoLight = document.querySelector(".logo-light");
 const logo = document.querySelector(".logo");
 const mMenutoggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
+const isFront = document.body.classList.contains("front-page");
 
 const lightModeOn = (event) => {
   navbar.classList.add("navbar-light");
-  logo.style.display = "block";
-  logoLight.style.display = "none";
 };
 const lightModeOff = (event) => {
-  navbar.classList.remove ("navbar-light");
-  logo.style.display = "none";
-  logoLight.style.display = "block";
+  navbar.classList.remove("navbar-light");
 };
 
-const openMenu = (event) => { 
+const changeNavHeight = (height) => {
+  navbar.style.height = height;
+};
+
+const openMenu = (event) => {
   // функция открывания меню
   menu.classList.add("is-open"); // вешает класс is-open
   mMenutoggle.classList.add("close-menu");
-  document.body.style.overflow="hidden"; // запрещает прокрутку сайта под меню
+  document.body.style.overflow = "hidden"; // запрещает прокрутку сайта под меню
   lightModeOn();
 };
-const closeMenu = (event) => { 
+const closeMenu = (event) => {
   // функция закрывания меню
   menu.classList.remove("is-open"); // убирает класс is-open
   mMenutoggle.classList.remove("close-menu");
-  document.body.style.overflow="hidden"; // возвращиет прокрутку сайта под меню
+  document.body.style.overflow = "hidden"; // возвращиет прокрутку сайта под меню
   lightModeOff();
 };
 
 window.addEventListener("scroll", () => {
-  this.scrollY > 1 ? lightModeOn() : lightModeOff();
+  this.scrollY > 1 ? changeNavHeight("4.5rem") : changeNavHeight("5.875rem");
+  if (isFront) {
+    this.scrollY > 1 ? lightModeOn() : lightModeOff();
+  }
 });
 mMenutoggle.addEventListener("click", (event) => {
   event.preventDefault();
-  menu.classList.contains ("is-open") ? closeMenu() : openMenu();
+  menu.classList.contains("is-open") ? closeMenu() : openMenu();
 });
 
-const swiperSteps = new Swiper (".steps-slider", {
+const swiperSteps = new Swiper(".steps-slider", {
   sped: 400,
   slidesPerView: 1,
   navigation: {
-    nextEl: '.steps-button-next',
-    prevEl: '.steps-button-prev',
+    nextEl: ".steps-button-next",
+    prevEl: ".steps-button-prev",
   },
   breakpoints: {
     // when window width is >= 320px
@@ -61,12 +65,12 @@ const swiperSteps = new Swiper (".steps-slider", {
   },
 });
 
-const swiper = new Swiper('.features-slider', {
+const swiper = new Swiper(".features-slider", {
   speed: 400,
   slidesPerView: 1,
   navigation: {
-    nextEl: '.slider-button-next',
-    prevEl: '.slider-button-prev',
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev",
   },
   breakpoints: {
     // when window width is >= 320px
@@ -88,33 +92,32 @@ const swiper = new Swiper('.features-slider', {
   },
 });
 
-const swiperBlog = new Swiper (".blog-slider", {
+const swiperBlog = new Swiper(".blog-slider", {
   speed: 400,
   slidesPerView: 2,
   spaceBetween: 30,
   navigation: {
-    nextEl: '.blog-button-next',
-    prevEl: '.blog-button-prev',
+    nextEl: ".blog-button-next",
+    prevEl: ".blog-button-prev",
   },
-
 });
 
 const modal = document.querySelector(".modal");
 const modalDialog = document.querySelector(".modal-dialog");
 
 document.addEventListener("click", (event) => {
- if (
-  event.target.dataset.toggle == "modal" ||
-  event.target.parentNode.dataset.toggle == "modal" || 
-  (!event.composedPath().includes(modalDialog) &&
-   modal.classList.contains("is-open"))
- ) {
-  event.preventDefault();
-  modal.classList.toggle("is-open");
- }
+  if (
+    event.target.dataset.toggle == "modal" ||
+    event.target.parentNode.dataset.toggle == "modal" ||
+    (!event.composedPath().includes(modalDialog) &&
+      modal.classList.contains("is-open"))
+  ) {
+    event.preventDefault();
+    modal.classList.toggle("is-open");
+  }
 });
 document.addEventListener("keyup", (event) => {
-  if(event.key == "Escape" && modal.classList.contains("is-open")) {
+  if (event.key == "Escape" && modal.classList.contains("is-open")) {
     modal.classList.toggle("is-open");
   }
 });
