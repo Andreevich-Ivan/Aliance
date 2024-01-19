2dc83f0c493fb5c856f179c3762e962f19f9c89a
Andreevich-Ivangit config --global user.name Andreevich-Ivangit config --global user.name Andreevich-Ivan <prostov2@gmail.com>
2024-01-19 15:26:31 +0300

    Refactoring modal

--- footer.php
+++ footer.php
@@ -26,7 +26,7 @@
                   id="user-phone"
                   type="tel"
                   name="userphone"
-                  class="input phone-mask"
+                  class="phone-mask input"
                   placeholder=" "
                   maxlength="30"
                   required
@@ -178,10 +178,72 @@
       </div>
       <!-- /.container -->
     </footer>
-    <div class="modal">
+    <div class="modal" id="feeback-modal">
       <div class="modal-dialog">
         <h2 class="modal-title">Есть вопросы?</h2>
-        <a href="#" class="modal-close">
+        <a href="#" class="modal-close" data-toggle="modal" data-target="#feedback-modal">
+          <svg class="close-icon" width="52" height="11">
+            <use href="img/sprite.svg#close"></use>
+          </svg>
+        </a>
+        <p class="modal-text">
+          Оставьте заявку, наш менеджер свяжется с Вами в ближайшее время
+          ответит на все интересующие вопросы и поможем даже в самых сложных
+          случаях!
+        </p>
+        <form action="handler.php" method="POST" class="modal-form">
+          <div class="input-group-wrapper input-group-vertical">
+            <div class="input-group modal-input-group">
+              <input
+                id="modal-user-name"
+                type="text"
+                class="input modal-input"
+                name="username"
+                placeholder=" "
+              />
+              <label
+                class="input-group-label modal-input-label"
+                for="modal-user-name"
+                >Имя
+            </label>
+            </div>
+            <div class="input-group modal-input-group">
+              <input
+                id="modal-user-phone"
+                type="tel"
+                class="input modal-input"
+                name="userphone"
+                placeholder=" "
+              />
+              <label
+                class="input-group-label modal-input-label"
+                for="modal-user-phone"
+                >Номер телефона</label
+              >
+            </div>
+          </div>
+          <div class="modal-form-footer">
+            <button type="submit" class="button modal-form-button">
+              Отправить заявку
+            </button>
+            <div class="notify">
+              <svg class="notify-icon" width="14" height="14">
+                <use href="img/sprite.svg#shield"></use>
+              </svg>
+              <p class="notify-text">
+                Обращаясь к нам вы получаете не только профессиональную работу,
+                но и абсолютную конфиденциальность информации!
+              </p>
+            </div>
+          </div>
+          <!-- /.cta-form-footer -->
+        </form>
+      </div>
+    </div>
+    <div class="modal" id="alert-modal">
+      <div class="modal-dialog">
+        <h2 class="modal-title">Спасибо</h2>
+        <a href="#" class="modal-close" data-toggle="modal" data-target="#alert-modal">
           <svg class="close-icon" width="52" height="11">
             <use href="img/sprite.svg#close"></use>
           </svg>
